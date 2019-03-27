fee65afcbf98b4107e83f368b3a468f47f20fe8c
Paul Biberstein <biberstein.paul@gmail.com>
2019-03-27 15:38:52 -0400

    Fixed chord parsing algorithm

--- src/StringChordParser.py
+++ src/StringChordParser.py
@@ -1,103 +1,82 @@
-import re, tables
+import re, tables, Chord
+import functools
 
 # takes a chord represented as a string and separates it into 3 components (also expands shorthand)
 def parseChord(stringChord):
+
+    root, intervals, bass = "", "", ""
+
     print("Parsing chord %s" % stringChord)
-    # if chord isn't formatted correctly, warn and break
-    #if not verifyChord(stringChord):
-    #   print("WARNING: chord %s is not in a parsable format" % stringChord)
-    #   return
-    
-    #try:
-    root, temp = stringChord.split(":")
-    if "/" in temp:
-        middle, bass = temp.split("/")
+    if "11" in stringChord or "13" in stringChord or "15" in stringChord:
+        print("Sorry, multidigit extension numerals aren't supported")
+        return
+
+    # split into root, middle section, and bass
+    if ":" in stringChord and "/" in stringChord:
+        root, middle, bass = stringChord.replace(":", "_").replace("/", "_").split("_")
+    elif ":" in stringChord:
+        root, middle = stringChord.split(":")
+    elif "/" in stringChord:
+        root, bass = stringChord.split("/")
     else:
-        middle = temp
-        bass = ""
+        root = stringChord
 
+    intervals = middleToIntervals(middle)
 
-    #intervals parsing
-    if middle[0] != "(":
-        #split into intervals and modifiers
-        shorthand = re.search(r".+?(?=(\(|$))", middle).group()
-        intervals = tables.intervals["shorthand"][shorthand]
-        modifiers = re.search(r"(\(.+|$)", middle).group()
+    return Chord.Chord(root, intervals, bass)
 
-        # convert modifiers to same format as intervals    
-        modifiers = modifierToTuple(modifiers)
-    else: 
-        #if no shorthand, set intervals equal to middle and modifiers to empty
-        intervals = middle
-        modifiers = ()
+# Convert String Intervals to tuple intervals
+def middleToIntervals(stringRep):
+    # shorthand and then intervals (has a '(' but doesn't start with it)
+    if stringRep[0] != "(" and "(" in stringRep:
+        shorthand, modifiers = stringRep.split("(")
+        modifiers = modifiers.replace(")","")
+        modifiers = modifiers.split(",")
 
-    #convert intervals to array so it is mutable 
-    intervals = [c for c in intervals]
-    modifiers = [c for c in modifiers]
+        intervals = tables.intervals["shorthandToIntervals"][shorthand]
 
-    # loop through modifiers.
-    # if modifier indicates sharping, flatting, or removing a chordal member
-    # then deal with it here.
-    # If it indicates adding a chordal member, deal with it in another loop
-    # after.
-    for i in range(len(modifiers)-1, -1, -1):
+        intervals = list(map(stringIntervalToTuple, intervals))
+        modifiers = list(map(stringIntervalToTuple, modifiers))
+        
+        intervals = applyModifiers(intervals, modifiers)
 
-        # alias just the number of the chordal member of the modifier
-        chordalMemberA = re.search(r"\d", modifiers[i]).group()
+    # just intervals (so it starts with "(" )
+    elif stringRep[0] == "(":
+        intervals = stringRep[1:-2]
+        intervals = intervals.split(",")
+        intervals = list(map(stringIntervalToTuple, intervals))
 
-        for j in range(len(intervals)-1, -1, -1):
-            # alias just the number of the chordal member of the current intervals
-            chordalMemberB = re.search(r"\d", intervals[j]).group()
+    # Just shorthand so it has no "("
+    elif "(" not in stringRep:
+        shorthand = stringRep
+        intervals = tables.intervals["shorthandToIntervals"][shorthand]
+        intervals = list(map(stringIntervalToTuple, intervals))
 
-            # if our modifier contains a step that is also in our current inervals,
-            # then update it (by reassigning or removing)
-            if chordalMemberA==chordalMemberB:
-                # if we want to remove a chordal member
-                if modifiers[i][0]=="*":
-                    intervals.pop(j)
-                # if we want to sharp or flat a chordal member
+    return intervals
+
+def applyModifiers(intervals, modifiers):
+    # Replace intervals that are flatted or sharped
+    for i in range(len(intervals)-1, -1, -1):
+        for j in range(len(modifiers)-1, -1, -1):
+            if intervals[i][0] == modifiers[j][0]:
+                if modifiers[j][1] == -1:
+                    intervals.pop(i)
+                    modifiers.pop(j)
+                    break
                 else:
-                    intervals[j] = modifiers[i]
-                # pop modifiers as we deal with them so we can deal with leftovers below
-                modifiers.pop(i)
-
-    # since we removed modifiers as we added them, the only ones
-    # remaining will be additional chordal members that we didn't deal with.
-    # Lets add them with the loop below
-    for interval in modifiers:
-        if "*" in interval:
-            raise Exception("ERROR: Attempt to remove a chordal member that doesn't exist (Chordal Member: %s" % interval)
-        intervals.append(interval)
-
-
-    return (root, intervals, bass)
-    #except:
-    #    raise Exception("The chord " + stringChord + " could not be parsed correctly")
-
-#helper function for main parser
-def modifierToTuple(modifierString):
-    if len(modifierString)==0:
-        return ()
-
-    # strip parentheses
-    modifierString = modifierString[1:-1]
-
-    # strip whitespace in case string is formatted wrong
-    modifierString = modifierString.replace(" ", "")
-
-    # split along commas to get individual elements
-    out = modifierString.split(",")
-
-    #convert to tuple
-    out = tuple(out)
+                    intervals[i] = modifiers[j]
+                    modifiers.pop(i)
+                    break
     
-    return out
-    
-def verifyChord(stringChord):
-    regex = r"[A-G][b#]?:.+?(\/\d)?"
+    for modifier in modifiers:
+        intervals.append(modifier)
 
-    match = re.search(regex, stringChord)
-    if match is not None and (match.span()[1]-match.span()[0]==len(stringChord)):
-        return True
+    return intervals
+
+def stringIntervalToTuple(stringInterval):
+    if stringInterval[0] == "*":
+        interval = (int(stringInterval[-1]), -1)
     else:
-        return False
+        interval = tables.intervals["intervalToTuple"][stringInterval]
+    
+    return interval
