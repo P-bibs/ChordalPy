fb0438e86b7040605758d0eba509dfa319432b47
Paul Biberstein <biberstein.paul@gmail.com>
2019-04-09 17:34:31 -0400

    Updated 'main' to use transpose alg. for real book

--- src/main.py
+++ src/main.py
@@ -1,10 +1,47 @@
-import Chord, tables, StringChordParser
+import os, traceback
+import Chord, Tables, StringChordParser, Transposers
 
-# various tester snippets
+# Input Directory
+PATH = "/Users/paulbiberstein/Desktop/Datasets/jazz_xlab"
+
+# Process files in a directory in Real Book format and return note arrays
 if __name__ == "__main__":
-    while True:
-        text = input("Type a chord")
+    
+    directory = os.listdir(PATH)
+
+    totalLines = 0
+    errorLines = 0
+    totalFiles = 0
+    errorFiles = 0
+
+    # Loop through each file and process
+    for file in directory:
+        # Only process data files
+        if ".xlab" not in file:
+            continue
+
+        totalFiles+=1
+
+        # Start by transposing the file.
+        # Returns a list of transposed chords
         try:
-            print(StringChordParser.parseChord(text).getSpelling())
+            transposedList = Transposers.transposeRealBookFile(path + "/" + file)
         except:
-            print("Sorry, chord couldn't be parsed")
+            errorFiles+=1
+            print("trouble with file " + file)
+
+        # Then read in each chord and convert to note Array
+        for chord in transposedList:
+            totalLines+=1
+            try:
+                StringChordParser.parseChord(chord).getNoteArray()
+            except:
+                errorLines+=1
+        
+        if totalFiles % 100 == 0:
+            print(str(totalFiles) + " files processed")
+
+    print("Errors: %s files out of %s total files" % (errorFiles, totalFiles))
+    print("Errors: %s lines out of %s total lines" % (errorLines, totalLines))
+
+        
