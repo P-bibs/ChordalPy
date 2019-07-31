def158df6da963ff88ecd8fd8864c1c1f18784f0
Paul Biberstein <biberstein.paul@gmail.com>
2019-07-30 22:39:08 -0400

    Slight update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Parse and manipulate musical chords",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/P-bibs/ChordalPyParser",
+    url="https://github.com/P-bibs/ChordalPy",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
